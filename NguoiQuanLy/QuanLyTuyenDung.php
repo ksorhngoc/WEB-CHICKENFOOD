13f97938be38334b24987acfe4c35a571fdce51d
ngoc <ksorhngockrpa@gmail.com>
2023-11-30 21:13:14 +0700

    update

--- NguoiQuanLy/QuanLyTuyenDung.php
+++ NguoiQuanLy/QuanLyTuyenDung.php
@@ -7,6 +7,7 @@
 <head>
 	<meta charset="utf8">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Tuyển Dụng</title>
     <style>
         .btnDonHang{
             width: 105px ; 
